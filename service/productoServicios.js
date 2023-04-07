a42c605f8d42d7d0697c7118b1695337b0a89763
María Sofía Fregona <94732342+sofifregona@users.noreply.github.com>
2023-04-06 23:39:35 -0300

    Adaptación CRUD
    Reemplazo del link de Heroku por una simple consulta a la base de datos alojada en github

--- service/productoServicios.js
+++ service/productoServicios.js
@@ -1,9 +1,10 @@
 const listaProductos = async () => {
   try {
     const respuesta = await fetch(
-      "https://sofia-tienda-de-ropa.herokuapp.com/productos"
+      "https://sofifregona.github.io/Sofia-tienda-de-ropa/db.json"
     );
-    return await respuesta.json();
+    const data = await respuesta.json();
+    return data.productos
   } catch (error) {
     return console.log(error);
   }
@@ -12,9 +13,12 @@ const listaProductos = async () => {
 const detalleProducto = async (id) => {
   try {
     const respuesta = await fetch(
-      `https://sofia-tienda-de-ropa.herokuapp.com/productos/${id}`
+      "https://sofifregona.github.io/Sofia-tienda-de-ropa/db.json"
     );
-    return await respuesta.json();
+    const data = await respuesta.json();
+    const productoEncontrado = data.productos.find(producto => producto.id === id);
+    console.log(productoEncontrado)
+    return productoEncontrado
   } catch (error) {
     return console.log(error);
   }
